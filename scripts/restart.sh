2d12b09c60a3570ba4bdfb24c73dd6fe09f2f83a
Thomas Venturini <thomas@venturini.codes>
2022-08-31 22:29:28 +0200

    fix issue in restart command

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,4 +4,4 @@
 source ./scripts/load_colors.sh
 
 # Bring the containers down.
-docker-compose down --remove-orphans && docker-compose upd -d && echo -e "${SUCCESS}It's dead now! 💀${NC}"
+docker-compose down --remove-orphans && docker-compose up -d && echo -e "${SUCCESS}It's dead now! 💀${NC}"
